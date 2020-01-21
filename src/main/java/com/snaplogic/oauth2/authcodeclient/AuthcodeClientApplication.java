7949347040af580cbc6541788f6cd0aec77632a6
kannan-sl <kchinnasamy@snaplogic.com>
2020-01-21 11:45:32 -0800

    Added basic code to support Outh2 Authorization code grandtype

---
+++ src/main/java/com/snaplogic/oauth2/authcodeclient/AuthcodeClientApplication.java
@@ -0,0 +1,13 @@
+package com.snaplogic.oauth2.authcodeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcodeClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthcodeClientApplication.class, args);
+    }
+
+}
